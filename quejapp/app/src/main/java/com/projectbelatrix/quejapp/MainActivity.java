b18c6ceb1702adebeb9d9524abf1b630377a710c
Lucas Farias <extintion007@gmail.com>
2017-01-21 17:47:18 -0300

    2341234
    saffsad

--- quejapp/app/src/main/java/com/projectbelatrix/quejapp/MainActivity.java
+++ quejapp/app/src/main/java/com/projectbelatrix/quejapp/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void lalala(View v){
+    public void lala2343(View v){
         Toast.makeText(this,"LALALAL", Toast.LENGTH_SHORT).show();
     }
 }
